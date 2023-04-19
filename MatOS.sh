12092a685bb8baf0ebf19d2590bb3b98f822b35b
Mato <96366252+Mato1111@users.noreply.github.com>
2023-04-19 00:08:26 -0400

    Update MatOS.sh

--- MatOS.sh
+++ MatOS.sh
@@ -7,8 +7,10 @@ sudo apt install nala -y
 sudo nala fetch --auto --fetches 4 -c US -c CA --https-only -y
 sudo nala upgrade -y
 sudo nala install neofetch htop latte-dock wget curl -y
-sudo flatpak install Discordapp Discord-screen Bottles -y
-curl -O https://raw.githubusercontent.com/bb010g/betterdisco rdctl/master/betterdiscordctl
+sudo snap remove Firefox
+sudo flatpak install Discordapp Discord-screen Bottles com.opera.Opera app/org.mozilla.firefox/x86_64/stable -y
+
+curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
 chmod +x betterdiscordctl
 sudo mv betterdiscordctl /usr/local/bin
 betterdiscordctl --d-install flatpak install
