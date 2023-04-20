d6f65b8e917fcbc5c735aab18975233607e299a4
Mato <96366252+Mato1111@users.noreply.github.com>
2023-04-19 21:58:12 -0400

    Update MatOS.sh

--- MatOS.sh
+++ MatOS.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 clear
-echo "This is the MatOS Post-Install script for Kubuntu! Please add Flathub in Discover before proceding just in case."
+echo "This is the MatOS Post-Install script for Kubuntu!"
 echo "Please install Black Box first, then continue the script."
 echo "If the system restarts, just run the script again."
 sudo apt install nala -y
 sudo nala fetch --auto --fetches 4 -c US -c CA --https-only -y
 sudo nala upgrade -y
-sudo nala install neofetch htop latte-dock wget curl -y
+sudo nala install neofetch htop latte-dock wget curl flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo snap remove Firefox
-sudo flatpak install Discordapp Discord-screen Bottles com.opera.Opera app/org.mozilla.firefox/x86_64/stable -y
+sudo flatpak install Discordapp Discord-screen Bottles com.opera.Opera app/org.mozilla.firefox/x86_64/stable app/com.valvesoftware.Steam/x86_64/stable -y --noninteractive
 
 curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
 chmod +x betterdiscordctl
