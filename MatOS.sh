744f28c58644dd6ac9796445c0f43ee98bce6755
Mato <96366252+Mato1111@users.noreply.github.com>
2023-04-21 10:15:24 -0400

    Update MatOS.sh

--- MatOS.sh
+++ MatOS.sh
@@ -16,6 +16,7 @@ sudo add-apt-repository ppa:kubuntu-ppa/backports -y
 
 sudo nala upgrade -y
 
+# Shit ton of apt packages that are mostly dependencies.
 sudo nala install neofetch htop wget curl flatpak qt5-style-kvantum qt5-style-kvantum-themes -y
 sudo nala install build-essential libvulkan-dev plasma-workspace-dev gstreamer1.0-libav \
 liblz4-dev libmpv-dev python3-websockets qtbase5-private-dev \
@@ -24,6 +25,7 @@ qml-module-qtwebchannel qml-module-qtwebsockets cmake -y
 sudo nala install libncursesw5-dev libssl-dev \
 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev python3.11 -y
 sudo nala install extra-cmake-modules qtdeclarative5-dev libqt5x11extras5-dev libkf5iconthemes-dev libkf5plasma-dev libkf5windowsystem-dev libkf5declarative-dev libkf5xmlgui-dev libkf5activities-dev build-essential libxcb-util-dev libkf5wayland-dev git gettext libkf5archive-dev libkf5notifications-dev libxcb-util0-dev libsm-dev libkf5crash-dev libkf5newstuff-dev libxcb-shape0-dev libxcb-randr0-dev libx11-dev libx11-xcb-dev kirigami2-dev libwayland-dev libwayland-client0 plasma-wayland-protocols libqt5waylandclient5-dev qtwayland5-dev-tools -y
+
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
 sudo snap remove firefox -y
@@ -36,8 +38,9 @@ betterdiscordctl --d-install flatpak install
 
 xdg-settings set default-web-browser opera.desktop
 
+
 # Wallpaper Engine Plugin (https://github.com/catsout/wallpaper-engine-kde-plugin)
-cd ~/
+cd
 # Download source
 git clone https://github.com/catsout/wallpaper-engine-kde-plugin.git
 cd wallpaper-engine-kde-plugin
@@ -58,7 +61,9 @@ make install_pkg
 # install lib
 sudo make install
 
-cd ~/
+
+# Latte Dock
+cd
 git clone https://github.com/KDE/latte-dock
 cd ~/latte-dock/
 bash install
@@ -72,4 +77,4 @@ clear
 neofetch
 echo "
 "
-echo "Set-up complete! Please restart the computer. Start Latte Dock after."
+echo "Set-up complete! Please restart the computer. Start Latte Dock after. Remember to install the widgets before applying the layout."
