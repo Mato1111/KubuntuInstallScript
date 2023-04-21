2e1201a8d89831ae9d46853c9b230042a75c8691
Mato <96366252+Mato1111@users.noreply.github.com>
2023-04-20 22:02:06 -0400

    Update MatOS.sh

--- MatOS.sh
+++ MatOS.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+ #!/bin/bash
 clear
 echo "This is the MatOS Post-Install script for Kubuntu!"
 echo "Please install Black Box first, then continue the script."
 echo "If the system restarts, just run the script again."
 echo"
-
-Please install these Plasma widgets after the running the cript, but before applying the Latte Dock layout: Latte Separator, Latte Spacer, Window Title Applet, Better Inline Clock
+Please install these Plasma widgets after the running the script, but before applying the Latte Dock layout: Latte Separator, Latte Spacer, Window Title Applet, Better Inline Clock
 "
 
 sudo apt install nala -y
@@ -25,13 +24,15 @@ sudo nala install libncursesw5-dev libssl-dev \
 libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev python3.11 -y
 
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo snap remove Firefox
-sudo flatpak install Discordapp Discord-screen Bottles grapejuice pupgui2 flatseal com.opera.Opera app/org.mozilla.firefox/x86_64/stable -y --noninteractive
+sudo snap remove firefox
+sudo flatpak install Discordapp Discord-screen Bottles pupgui2 flatseal com.opera.Opera app/org.mozilla.firefox/x86_64/stable -y --noninteractive
 
 curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
 chmod +x betterdiscordctl
 sudo mv betterdiscordctl /usr/local/bin
 betterdiscordctl --d-install flatpak install
+
+xdg-settings set default-web-browser opera.desktop
 
 # Wallpaper Engine Plugin (https://github.com/catsout/wallpaper-engine-kde-plugin)
 cd ~/
@@ -57,7 +58,6 @@ sudo make install
 
 cd ~/Desktop
 wget https://raw.githubusercontent.com/Mato1111/KubuntuInstallScript/main/Default_-_2.layout.latte
-mv main/Default_-_2.layout.latte.1 main/Default_-_2.layout.latte
 latte-dock
 xdg-open https://store.steampowered.com/about/
 xdg-open https://store.kde.org/p/1325246/
@@ -65,8 +65,5 @@ xdg-open https://store.kde.org/p/1325246/
 clear
 neofetch
 echo "
-
-
-
 "
 echo "Set-up complete! Please restart the computer."
